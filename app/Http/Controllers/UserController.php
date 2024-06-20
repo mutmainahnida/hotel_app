191f467737ab37160eaad9930a52489a2d23f173
mutmainahnida <nidaaa.m03@gmail.com>
2024-06-20 12:43:25 +0700

    saya menambahkan create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+
+    //bagian untuk menampilkan create
+    public function create()
+    {
+        $roles = Role::all();
+        $data = [
+            "roles" => $roles
+        ];
+        return view('user.create',$data);
+    }
+
+    //bagian untuk store create
+    public function store(Request $request)
+{
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8|confirmed',
+    ]);
+
+    User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'role_id'   => $request->role_id
+        // Assign role_id if applicable
+    ]);
+
+    return redirect()->route('user.index')->with('success', 'User created successfully');
+}
+
 }
